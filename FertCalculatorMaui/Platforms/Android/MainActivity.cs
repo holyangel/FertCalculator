cdca67ca9bb1e82eaa5a1f9b2645b81278df2266
HolyAngel <slverwolf@gmail.com>
2025-03-16 10:06:47 -0500

    Remove Mac support. Fix Android Namespace

--- FertCalculatorMaui/Platforms/Android/MainActivity.cs
+++ FertCalculatorMaui/Platforms/Android/MainActivity.cs
@@ -1,8 +1,8 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
-namespace FertCalculatorMaui;
+namespace FertCalculatorMaui.Platforms.Android;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
--- FertCalculatorMaui/Platforms/Android/MainApplication.cs
+++ FertCalculatorMaui/Platforms/Android/MainApplication.cs
@@ -1,7 +1,7 @@
-﻿using Android.App;
+using Android.App;
 using Android.Runtime;
 
-namespace FertCalculatorMaui;
+namespace FertCalculatorMaui.Platforms.Android;
 
 [Application]
 public class MainApplication : MauiApplication
